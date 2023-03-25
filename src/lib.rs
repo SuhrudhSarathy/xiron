7e81a958a0e988f33b2d2826fbc898700a735a18
Suhrudh <suhrudhsarathy@gmail.com>
2023-03-25 16:58:16 +0530

    added comms

--- src/lib.rs
+++ src/lib.rs
@@ -5,3 +5,5 @@ pub mod parameter;
 pub mod parser;
 pub mod prelude;
 pub mod utils;
+pub mod handler;
+pub mod comms;
--- src/object/mod.rs
+++ src/object/mod.rs
@@ -1,11 +1,9 @@
-pub mod handler;
 pub mod robot;
 pub mod sensors;
 pub mod static_obj;
 pub mod wall;
 pub mod dynamic_obj;
 
-pub use handler::*;
 pub use robot::*;
 pub use sensors::*;
 pub use static_obj::*;
